25325491279f3b013b5e6d45aabb6ef860493e37
HarryRunham <harryrunham@gmail.com>
2022-11-07 22:31:39 +0000

    Update Bets.py
    Waited too long before making another commit, should've split this into multiple commits.
    
    Removed unnecessary self-credit comment
    Added new exit system, added exit options at each input request
    Added support for other popular odds formats (including format choice reset option) - user is no longer required to convert odds themselves. In the process of this streamlined previously messy logic in input section
    Streamlined how whitespace lines are printed
    Added docstrings to old and new functions
    Revamped comments
    
    Several variables renamed/deleted/used in different ways.

--- Bets.py
+++ Bets.py
@@ -1,21 +1,24 @@
 # NOTES
-# 
-# When done add comments.
 #
 # Could improve MatrixPrint but at that point I think using TextTable would just be better
+#
+# Could add option to delete rows of the all_results matrix to remove results obtained by incorrectly input odds.
+#
+# Could add support for fractional odds to be entered as integers where appropriate - I anticipate this needing quite a lot of code, so it's probably not worth it, at least for now.
 # 
-# Could add a converter so the user doesn't have to convert the odds themselves. Should really do this.
+# The input section has 3 main branches of the if statement, each handling a different format of odds. As each of these branches are similar, only the decimal branch is fully commented.
 # 
-# Am going to make huge changes to INPUT RECEPTION, PROCESSING AND VALIDATION to complete the above. Upload to GitHub before doing this.
-# 
-# TEST REPOSITORY COMMIT
+# Explanation of the mathematics of the analysis itself will soon be available in the README. 
 
 # ------------------------------
 # FUNCTIONS
 # ------------------------------
 
 def MatrixPrint(list):
-    print("Name, Team 1 Bet Criterion, Team 2 Bet Criterion")
+    '''
+    Prints rows of a matrix with visually labelled columns.
+    '''
+    print("\n" + "Name, Team 1 Bet Criterion, Team 2 Bet Criterion")
     i = 0
     while i < len(list):
         j = 0
@@ -26,96 +29,220 @@ def MatrixPrint(list):
         print(row)
         i += 1
 
+def IsValid(float_1, float_2):
+    '''
+    Checks for the decimal forms of the two values in an odds pair to be both more than or equal to 1 - odds values of less than 1 are impossible.
+    '''
+    if float_1 >= 1 and float_2 >= 1:
+        return True
+    else:
+        print("\n" + "The odds values are invalid. Please re-enter the odds. ")
+        return False
+
+def ConvertFromFractional(float_1, float_2):
+    '''
+    Converts a fractional odds value float_1/float_2 to decimal format.
+    '''
+    return float_1/float_2 + 1
+
+def ConvertFromMoneyline(float):
+    '''
+    Converts a moneyline odds value to decimal format.
+    '''
+    if float > 0:
+        return float/100 + 1
+    else:
+        return -100/float + 1
+
+def ExitProgram():
+    '''
+    Prints a goodbye message and exits the program.
+    '''
+    print("\n" + "E entered. Thankyou for using this betting analyser. Good luck with your bets! " + "\n")
+    raise SystemExit
+
 # ------------------------------
 # INITIALISATION AND WELCOME
 # ------------------------------
 
-program_exit_confirmation = False
 all_results_matrix = []
 
-print(3*"\n")
-print("Welcome to your betting analyser. ")
+print(3*"\n" + "Welcome to your betting analyser. ")
+print("Enter E at any point to exit the program. ")
 
 # ------------------------------
 # MAIN BODY
 # ------------------------------
 
-while program_exit_confirmation == False:
-
+while True:
+    
 # ------------------------------
 # INPUT RECEPTION, PROCESSING AND VALIDATION
 # ------------------------------
     
-    print()
-    print("If the odds you are presented with are fractional, represent that fraction as a decimal and add 1 to achieve its decimal form. ") # DELETE AFTER DIFFERENT FORMS SUPPORTED
-    print("If you are presented with moneyline odds x, represent x/100 as a decimal and add 1 to achieve its decimal form. ") # DELETE AFTER DIFFERENT FORMS SUPPORTED
-
-    variable_check_done = False
-    value_check_done = False
+    format_chosen = False
     
-
-
-    # print("Decimal odds look like: 1.45, 2.55")
-    # print("Fractional odds look like: 45/100, 155/100")
-    # print ("Moneyline odds look like: +45, +155") - NVM, HOW I THOUGHT THEY WORKED WAS INCORRECT, NEED TO RECTIFY ESPECIALLY BEFORE ADDING A CONVERTER, USE https://www.aceodds.com/bet-calculator/odds-converter.html
-    # odds_form_chosen = False
-    # while odds_form_chosen = False:
-    #     odds_form_choice = input("What form are the odds in? ")
-    # SWITCH CASE STATEMENT (OR EQUIVALENT) HERE, TO CHOOSE BETWEEN WHICH WHILE LOOP TO USE TO PROCESS THE INPUT - THEN HAVE THE VALUE CHECK AS BEING SEPARATE AFTER THE VARIABLE CHECK WHILE LOOPS (THERE SHOULD BE 3 OF THEM) 
-
-
-    while variable_check_done == False or value_check_done == False: # This loop can only be exited if both the variables are true. value_check_done can only become true if variable_check_done is true. therefore you could have this as simply being value_check_done == False, but I will leave it as is for code readability.
-
-        variable_check_done = False
-        value_check_done = False
+    while format_chosen == False:
         
-        print()
-        odds1_raw = input("Enter the odds for team 1 in decimal form: ")
-        odds2_raw = input("Enter the odds for team 2 in decimal form: ")
+        input_validated = False
         
-        try:
-            # Try converting input into floats.
-            odds1 = float(odds1_raw)
-            odds2 = float(odds2_raw)
-        except:
-            # The values input failed to be converted; the user messed up in some way.
-            try:
-                # Strip input of all characters except for digits and periods, then try converting into floats.
-                odds1 = float("".join(filter(lambda x: x.isdigit() or x == ".", odds1_raw)))
-                odds2 = float("".join(filter(lambda x: x.isdigit() or x == ".", odds2_raw)))
-            except:
-                # One or more of the values contained more than one period, or no period at all. Not worth going through every possible value the user could have meant to input - just return to ask for input again.
-                print("\n" + "Both inputs must contain digits and exactly one period each.")
-            else:
-                # The values were succesfully cleaned. Check that the result is the values the user meant to submit.
-                confirmation_check_done = False
-                while confirmation_check_done == False:
-                    confirmation = input("\n" + "Are these the correct odds? Enter Y or N. Value 1: " + str(odds1) + " Value 2: " + str(odds2) + " ")
-                    if confirmation == "Y":
-                        confirmation_check_done = True
-                        variable_check_done = True
-                        # This allows both while loops to end, releasing control to the rest of the program.
-                    elif confirmation == "N":
-                        confirmation_check_done = True
-                        # variable_check_done remains False, allowing the user to enter new odds.
+        print("\n" + "Decimal odds look like: 1.45, 2.55")
+        print("Fractional odds look like: 45/100, 155/100")
+        print("Moneyline odds look like: -222, +155")
+
+        format_input = input("\n" + "What format are the odds in? Type D for decimal, F for fractional, and M for moneyline. Enter R to reset format choice. ")
+        
+        if format_input == "D":
+            format_chosen = True
+            while input_validated == False:
+                odds1_raw = input("\n" + "Enter the odds for team 1 in decimal form: ")
+                odds2_raw = input("Enter the odds for team 2 in decimal form: ")
+                if odds1_raw == "E" or odds2_raw == "E":
+                    ExitProgram()
+                elif odds1_raw =="R" or odds2_raw == "R":
+                    format_chosen = False
+                else:
+                    try:
+                        odds1 = float(odds1_raw)
+                        odds2 = float(odds2_raw)
+                    except: # The values input are not floats; the user messed up in some way.
+                        try: # Attempt to rectify input.
+                            odds1 = float("".join(filter(lambda x: x.isdigit() or x == ".", odds1_raw)))
+                            odds2 = float("".join(filter(lambda x: x.isdigit() or x == ".", odds2_raw)))
+                        except: # One or more of the values contained more than one period. Not worth going through every possible value the user could have meant to input, so return to ask for input again (achieved by leaving input_validated as False)
+                            print("\n" + "Both inputs must contain digits and at most one period each.")
+                        else: # After cleaning the input resembles floats. Check that these floats are the values the user meant to submit.
+                            confirmation_check_done = False
+                            while confirmation_check_done == False:
+                                confirmation = input("\n" + "Are these the correct odds? Enter Y or N. Value 1: " + str(odds1) + " Value 2: " + str(odds2) + " ")
+                                if confirmation == "Y":
+                                    confirmation_check_done = True
+                                    if IsValid(odds1, odds2) == True:
+                                        input_validated = True
+                                elif confirmation == "N":
+                                    confirmation_check_done = True # input_validated remains False, allowing the user to enter new odds.
+                                elif confirmation == "E":
+                                    ExitProgram()
+                                elif confirmation == "R":
+                                    confirmation_check_done = True
+                                    format_chosen = False
+                                else:
+                                    print("\n" + "Please enter Y or N.") # confirmation_check_done remains False, prompting the user to respond correctly.
+                    else: # The values input were successfuly converted into floats. Assume correct input.
+                        if IsValid(odds1, odds2) == True:
+                            input_validated = True
+        
+        elif format_input == "F":
+            format_chosen = True
+            while input_validated == False:
+                print("\n" + "When entering the odds, make sure to include a forward slash: / ") # For now I will require fractional odds such as 3 to be written as 3/1.
+                odds1_raw = input("\n" + "Enter the odds for team 1 in fractional form: ")
+                odds2_raw = input("Enter the odds for team 2 in fractional form: ")
+                if odds1_raw == "E" or odds2_raw == "E":
+                    ExitProgram()
+                elif odds1_raw =="R" or odds2_raw == "R":
+                    format_chosen = False
+                else:
+                    try:
+                        odds1_numerator, odds1_denominator = odds1_raw.split("/")
+                        odds2_numerator, odds2_denominator = odds2_raw.split("/")
+                    except:
+                        print("\n" + "Both inputs must contain exactly one / each. ")
                     else:
-                        print("\n" + "Please enter Y or N." + "\n")
-                        # confirmation_check_done remains False, prompting the user to respond correctly.
-        else:
-            # The values input were successfuly converted into floats. Assume correct input.
-            variable_check_done = True
+                        try:
+                            odds1_numerator = float(odds1_numerator)
+                            odds1_denominator = float(odds1_denominator)
+                            odds2_numerator = float(odds2_numerator)
+                            odds2_denominator = float(odds2_denominator)
+                        except:
+                            try:
+                                odds1_numerator = float("".join(filter(lambda x: x.isdigit() or x == ".", odds1_numerator)))
+                                odds1_denominator = float("".join(filter(lambda x: x.isdigit() or x == ".", odds1_denominator)))
+                                odds2_numerator = float("".join(filter(lambda x: x.isdigit() or x == ".", odds2_numerator)))
+                                odds2_denominator = float("".join(filter(lambda x: x.isdigit() or x == ".", odds2_denominator)))
+                            except:
+                                print("\n" + "Please ensure that the numerators and denominators of the fractions contain one period at most, and otherwise only contain digits. ")
+                            else:
+                                confirmation_check_done = False
+                                while confirmation_check_done == False:
+                                    confirmation = input("\n" + "Are these the correct odds? Enter Y or N. Value 1: " + str(odds1_numerator) + "/" +  str(odds1_denominator) + " Value 2: " + str(odds2_numerator) + "/" + str(odds2_denominator) + " ")
+                                    if confirmation == "Y":
+                                        confirmation_check_done = True
+                                        odds1 = ConvertFromFractional(odds1_numerator, odds1_denominator)
+                                        odds2 = ConvertFromFractional(odds2_numerator, odds2_denominator)
+                                        if IsValid(odds1, odds2) == True:
+                                            input_validated = True
+                                    elif confirmation == "N":
+                                        confirmation_check_done = True
+                                    elif confirmation == "E":
+                                        ExitProgram()
+                                    elif confirmation == "R":
+                                        confirmation_check_done = True
+                                        format_chosen = False
+                                    else:
+                                        print("\n" + "Please enter Y or N." + "\n")
+                        else:
+                            odds1 = ConvertFromFractional(odds1_numerator, odds1_denominator)
+                            odds2 = ConvertFromFractional(odds2_numerator, odds2_denominator)
+                            if IsValid(odds1, odds2) == True:
+                                input_validated = True
         
-        if variable_check_done == True:
-            if odds1 >= 1 and odds2 >= 1:
-                value_check_done = True
-            else:
-                print("\n" + "The odds are invalid - they should both be more than or equal to 1. Please re-enter the odds. ")
+        elif format_input == "M":
+            format_chosen = True
+            while input_validated == False:
+                odds1_raw = input("\n" + "Enter the odds for team 1 in moneyline form: ")
+                odds2_raw = input("Enter the odds for team 2 in moneyline form: ")
+                if odds1_raw == "E" or odds2_raw == "E":
+                    ExitProgram()
+                elif odds1_raw =="R" or odds2_raw == "R":
+                    format_chosen = False
+                else:
+                    try:
+                        odds1 = float(odds1_raw)
+                        odds2 = float(odds2_raw)
+                    except:
+                        try:
+                            odds1 = float("".join(filter(lambda x: x.isdigit() or x == "." or x == "-", odds1_raw)))
+                            odds2 = float("".join(filter(lambda x: x.isdigit() or x == "." or x == "-", odds2_raw)))
+                        except:
+                            print("\n" + "Both inputs must contain digits and at most one period each.")
+                        else:
+                            confirmation_check_done = False
+                            while confirmation_check_done == False:
+                                confirmation = input("\n" + "Are these the correct odds? Enter Y or N. Value 1: " + str(odds1) + " Value 2: " + str(odds2) + " ")
+                                if confirmation == "Y":
+                                    confirmation_check_done = True
+                                    odds1 = ConvertFromMoneyline(odds1)
+                                    odds2 = ConvertFromMoneyline(odds2)
+                                    if IsValid(odds1, odds2) == True:
+                                        input_validated = True
+                                elif confirmation == "N":
+                                    confirmation_check_done = True
+                                elif confirmation == "E":
+                                    ExitProgram()
+                                elif confirmation == "R":
+                                    confirmation_check_done = True
+                                    format_chosen = False
+                                else:
+                                    print("\n" + "Please enter Y or N." + "\n")
+                    else:
+                        odds1 = ConvertFromMoneyline(odds1)
+                        odds2 = ConvertFromMoneyline(odds2)
+                        if IsValid(odds1, odds2) == True:
+                            input_validated = True
+        
+        elif format_input == "R":
+            print("\n" + "This option is for use once a format has already been chosen. ") # The reset format choice option is not meant to be used at this juncture, as no format has been chosen yet.
+        
+        elif format_input == "E":
+            ExitProgram()
+        
+        else:
+            print("\n" + "Invalid input. ")
 
 # ------------------------------
 # ANALYSIS CALCULATIONS
 # ------------------------------
-
-    # Analysis algorithm is my own work.
 
     team1_bet_criterion = 100*(1/odds1)
     team1_bet_criterion_formatted = round(team1_bet_criterion, 2)
@@ -133,8 +260,9 @@ while program_exit_confirmation == False:
     
     team2_bet_criterion_formatted = str(team2_bet_criterion_formatted) + str("%")
 
-    print()
-    name_of_site = input("Enter a name for this analysis. ")
+    name_of_site = input("\n" + "Enter a name for this analysis. Entering E will exit the program. ")
+    if name_of_site == "E":
+        ExitProgram()
 
     all_results_matrix.append([name_of_site, "p > " + team1_bet_criterion_formatted, "p < " + team2_bet_criterion_formatted])
 
@@ -142,26 +270,21 @@ while program_exit_confirmation == False:
 # ANALYSIS RESULT OUTPUT
 # ------------------------------
 
-    print("\n" + "The following probabilities are rounded to 2 decimal places. ")
+    print(3*"\n" + "The following probabilities are rounded to 2 decimal places. ")
     print("Let p be the probability of team 1 winning. ")
     print("\n" + "Bet on team 1 if you believe p is more than " + team1_bet_criterion_formatted)
     print("Bet on team 2 if you believe p is less than " + team2_bet_criterion_formatted)
 
     print("\n" + "All other things remaining equal, you should bet on the site that provides the best odds. All analysis results so far: ")
 
-    print()
     MatrixPrint(all_results_matrix)
 
     program_exit_check_done = False
-
     while program_exit_check_done == False:
         program_exit_answer = input("\n" + "Would you like to continue or exit the program? Enter C or E. ")
         if program_exit_answer == "E":
-            program_exit_check_done = True
-            program_exit_confirmation = True
+            ExitProgram()
         elif program_exit_answer == "C":
             program_exit_check_done = True
         else:
-            print("\n" + "Please enter C or E.")
-
-print("\n" + "Thankyou for using this betting analyser. Good luck with your bets! ")
+            print("\n" + "Please enter C or E. ")
